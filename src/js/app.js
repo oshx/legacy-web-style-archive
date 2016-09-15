7dfb48e5ac84dbd20bd2d10748bbdeeeef6f3b3b
오승환 <oshios@naver.com>
2016-09-15 12:36:17 +0000

    setting change for bower components

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 var studyRequire = require.config({
     baseUrl: './src/js/',
     paths: {
-        jquery: ['https://code.jquery.com/jquery-1.12.4.min', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min'],
-        Spinner: 'https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.3.2/spin.min'
+        jquery: ['../../bower_components/jquery/dist/jquery.min','https://code.jquery.com/jquery-1.12.4.min', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min'],
+        Spinner: ['../../bower_components/spin.js/spin.min','https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.3.2/spin.min']
     }
 });
 studyRequire(['main'], function(main){
